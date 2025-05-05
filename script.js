181910f96a89245f34451e5f5010c0deda1918f9
Sunshin818 <buzhishuiyan@gmail.com>
2025-05-05 22:31:57 +0800

    更新

---
+++ script.js
@@ -0,0 +1,27 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+
+// 自动切换每3秒
+setInterval(nextSlide, 3000);
